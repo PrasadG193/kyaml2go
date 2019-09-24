3583e7b1dbb9d923e3929c94a225065b8d6d9bdd
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-09-24 13:23:06 +0530

    Makefile - Add rule to build http server
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- cmd/serve/server.go
+++ cmd/serve/server.go
@@ -10,9 +10,11 @@ import (
 )
 
 const API_VERSION = "v1"
+const PORT = "8080"
 
 func main() {
 	router := httprouter.New()
+	log.Printf("server started accepting requests on port=%s..\n", PORT)
 	router.POST("/v1/convert", serve.HandleConvert)
 	router.POST(fmt.Sprintf("/convert", API_VERSION), serve.HandleConvert)
 	log.Fatal(http.ListenAndServe(":8080", router))
