294a42fa9fcaabc1560f524a1d632cedc98ce47f
Prasad Ghangal <prasad.ghangal@gmail.com>
2019-11-27 23:49:54 +0530

    Update frontend
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

--- frontend/index.js
+++ frontend/index.js
@@ -1,50 +1,72 @@
-const URL = "http://localhost:8080/v1/convert?method="
+//const URL = "http://localhost:8080/v1/convert"
+const URL = "https://us-central1-yaml2go.cloudfunctions.net/kgoclient-gen?method=create"
 
 let go = document.getElementById("goGenerator")
 
+let editor = ""
 
-window.generatorCall=function (api){
-  let yamlData  = document.getElementById("yamlGenerator").value
-  document.getElementById('yamlGenerator').style.border = "1px solid #ced4da"
-  console.log(yamlData)
+window.generatorCall=function (){
+  let yamlData  = document.getElementById("codegen").value
+  document.getElementById('codegen').style.border = "1px solid #ced4da"
+  yamlData = editor.getValue()
   $.ajax({
-    'url' : `${URL}${api}`,
+    'url' : `${URL}`,
     'type' : 'POST',
     'data' : yamlData,
     'success' : function(data) { 
-        go.value = data
+        document.getElementById("error").style.display="none" 
+        document.getElementById("err-span").innerHTML="";     
+        go.setValue(data)
     },
-    'error' : function(request,error)
-    { 
-      document.getElementById('yamlGenerator').style.border = "1px solid red"
-      alert('Something is fishy')      
+    'error' : function(jqXHR, request,error)
+    {
+      document.getElementById('codegen').style.border = "1px solid red"
+      if (jqXHR.status == 400) {
+        displayError('Invalid yaml format')
+      } else {
+        displayError('Something went wrong! Please report this to me@prasadg.dev')
+      }
     }
   });
 
 }
 
-//Create Function
-document.getElementById("createGO").addEventListener('click', ()=>{
-   generatorCall('create')
-})
-
-//Update Function
-document.getElementById("updateGO").addEventListener('click', ()=>{
-  generatorCall('update')
-})
-
-//Delete Function
-document.getElementById("deleteGO").addEventListener('click', ()=>{
-    generatorCall('delete')
+//Convert
+document.getElementById("convert").addEventListener('click', ()=>{
+   generatorCall()
 })
 
 //Clear YAML
 document.getElementById('clearYaml').addEventListener('click',()=>{
-  document.getElementById("yamlGenerator").value = ''
+  editor.setValue('')
 })
 
 //Clear Go
-
 document.getElementById('clearGo').addEventListener('click',()=>{
-  document.getElementById("goGenerator").value = ''
+  go.setValue('')
 })
+
+
+$(document).ready(function(){
+    //code here...
+    var input = $(".codemirror-textarea")[0];
+    var output = $(".codemirror-textarea")[1];
+    editor = CodeMirror.fromTextArea(input, {
+        mode: "text/x-yaml",
+    	lineNumbers : true
+    });
+
+    go = CodeMirror.fromTextArea(output, {
+    	lineNumbers : true,
+        mode: "text/x-go"
+    });
+
+    editor.setValue('# Paste your k8s spec yaml here...\n')
+
+    go.setValue('// Go\n')
+});
+
+function displayError(err){
+  document.getElementById("err-span").innerHTML=err;
+  document.getElementById("error").style.display="block"
+}
